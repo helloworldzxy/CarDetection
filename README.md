276b6a8033b3895104fe2526a3ea3caa7bbe6ce7
helloworldzxy <zxy_bupt@sina.com>
2017-04-05 09:37:13 +0800

    Router for targetSearch

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # CarDetection
 
-#### 4.1
+### 4.1
 
 练习使用Webpack。
 
@@ -18,16 +18,34 @@ These files in CarDetection/practice_webpack:
 
 are created for webpack learning practice [Webpack中文指南](http://webpackdoc.com/usage.html).
 
-#### 4.4 
+### 4.4 
 
 用Express + Webpack + React.js 搭建了环境并跑通了系统，实现了第一个React Component: headerBanner.js。
 
-注意：
+#### 注意：
 
 - Webpack.config.js配置项中每一项的用途。
 - 路径引用和路径配置问题。
     import组件、require CSS等文件时的相对路径./ ../。
     webpack.config.js中的output配置项与express工程下的配置static路径的对应关系。
 - 目录结构，尤其是public/javascripts/下的目录结构设计，将routers，components分开，有利于模块化，组件化编程。写好的组件统一引入到/routers/中的相应页面。
+- 使用webpack plugin--HtmlWebpackPlugin，可以将你自己写的template.jade转换成相应的html文件并自动引入相应的打包后生成的css和js文件。
+
+#### 添加导航栏组件navigation.js:
+这个组件与headBanner.js的定义方式不同：
+
+后者是直接将jsx表达式作为一个箭头函数的返回值赋给一个const常量HeaderBanner然后export。而前者是，定义一个“class”(ES6),将jsx表达式作为该class的render()函数的返回值，这个class继承自Component.所以在targetSearch.js中import该组件的方式也不一样：
+import const时需要大括号而import navigation.js不需要大括号。
+
+- 如何实现导航栏项目单击时的背景色变化？
+- 为什么要用class定义navigation组件？
+用class...extends定义navigation组件，可以在组件中定义ComponentDidMount函数，绑定一些事件处理函数，实现单击更换背景颜色等功能。
+
+- react-router与Express都可以路由，那区别是？
+
+- searchTarget.js中，可以用`ReactDOM.render`实现页面布局，为什么要用`class TargetSearch extends Component`定义一个TargetSearch组件？
+需要路由，用react-router;方便组件化。
+
+- 在`ReactDOM.render()`的第一个参数中用Router实现路由后，访问相应的url如http://localhost:3002/targetSearch#/nonmotor可以访问到，但是单击按钮访问不到--在li的a标签中也要相应设置`href="/targetSearch#/motor"`等，表示url跳转到/targetSearch#/motor等。如果href不设置，只设置Router，则单击后跳转的url为localhost:3002/motor，就不是相对/targetSearch跳转了。相当于：Router的设置，是设置每个url对应的页面应该渲染成的样子；href是控制单击链接时的跳转。
 
 
--- public/javascripts/routers/targetSearch.js
+++ public/javascripts/routers/targetSearch.js
@@ -1,14 +1,20 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import { Router, Route, IndexRoute, hashHistory } from 'react-router';
 
 import { HeaderBanner } from '../components/display/headerBanner.js';
+import Navigation from '../components/display/navigation.js'; //注意二者引入方式，一个带大括号一个不带
 
 require('../../stylesheets/common.css');
 
-class TargetSearch extends Component {
+class TargetSearch extends Component { //该class继承自Component, 用于下面的Route中
 	render() { //这里用的是大括号 包裹着函数体 与display/headerBanner.js对比
 		return <div>
 			<HeaderBanner/>
+			<div className="container">
+				<Navigation/>
+				<div className="content">{this.props.children}</div>
+			</div>
 		</div>;
 	}
 }
@@ -18,6 +24,35 @@ class TargetSearch extends Component {
 // 	<h1>Hello world~</h1>
 // , document.getElementById('main'));
 
+//不需要路由时可以这么写
+// ReactDOM.render(
+// 	<div>
+// 		<HeaderBanner/> 
+// 		<div className="container">
+// 			<Navigation/>
+// 			<div className="content">targetSearch content targetSearch content targetSearch content</div>
+// 		</div>
+// 	</div>
+ 
+// , document.getElementById("main"));
+
+const Motor = () => { return <div><div>MotorMotorMotorMotorMotorMotor</div></div>; };
+const NonMotor = () => { return <div><div>NonMotorNonMotorNonMotorNonMotorNonMotorNonMotor</div></div>; };
+const Staff = () => { return <div><div>StaffStaffStaffStaffStaff</div></div>; };
+
 ReactDOM.render(
-	<HeaderBanner/>
-, document.getElementById("main"));
+	<Router history={hashHistory}>
+		<Route path="/" component={TargetSearch}>
+			<IndexRoute component={Motor}/>
+			<Route path="/motor" component={Motor}/>
+			<Route path="/nonMotor" component={NonMotor}/>
+			<Route path="/staff" component={Staff}/>
+		</Route>
+	</Router>
+, document.getElementById('main'));
+
+
+
+
+
+
